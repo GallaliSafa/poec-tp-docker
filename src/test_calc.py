29a61162e51537a81e9d02db6ab98cd13fdcdd6f
Safa <GallaliSafa@gmail.com>
2023-11-08 15:32:06 +0100

    seconde commit

--- src/test_calc.py
+++ src/test_calc.py
@@ -2,24 +2,29 @@ import unittest
 from calc import apply_vat
 class TestCalcMethods(unittest.TestCase):
     def test_price(self):
-        self.assertEqual(apply_vat(100,20))
+        self.assertEqual(apply_vat(100,20),120)
 
-    def test_price(self):
-        self.assertEqual(apply_vat(55.25,5.5))
+    def test_float(self):
+        self.assertEqual(apply_vat(55.25,5.5),58.28874999999999)
 
-        def test_price(self):
-            self.assertEqual(apply_vat(0, 10))
+    def test_null(self):
+        with (self.assertRaises(ValueError)):
+            apply_vat(0,10)
 
-    def test_price(self):
-        self.assertEqual(apply_vat(-10.99, 10))
+    def test_prix_negatif(self):
+        with self.assertRaises(ValueError):
+            apply_vat(-10.99, 10)
 
-    def test_price(self):
-        self.assertEqual(apply_vat( 'wrong value', 10))
+    def test_chaine(self):
+        with self.assertRaises(ValueError):
+            apply_vat('wrong value', 10)
 
-    def test_price(self):
-        self.assertEqual(apply_vat(100, -10))
+    def test_percent_negatif(self):
+        with self.assertRaises(ValueError):
+            apply_vat(100, -10)
 
-        def test_price(self):
-            self.assertEqual(apply_vat(100, 110))
+    def test_tva(self):
+        with self.assertRaises(ValueError):
+            apply_vat(100, 110)
 if __name__ == '__main__':
     unittest.main()
